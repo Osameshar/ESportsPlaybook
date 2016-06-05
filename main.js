c626a5f2ebb74f8236da28f99f741c5d45fba924
Corey Keller <cakeller43@gmail.com>
2016-06-04 23:43:26 -0500

    Basic Layout and Navigation

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+require("electron-reload")(__dirname)
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600
-  })
+  mainWindow = new BrowserWindow({width: 1280, height: 720, minWidth: 800, minHeight: 500})
   
   mainWindow.loadURL(`file://${__dirname}/index.html`)
   
@@ -29,7 +28,6 @@ function createWindow() {
   app.on('window-all-closed', function () {
       app.quit();
   })
-
 
 
 // In this file you can include the rest of your app's specific main process
