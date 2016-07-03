a68cf8c24c3ea12923be4d68aaff0f41582046ea
Corey Keller <cakeller43@gmail.com>
2016-07-03 11:24:44 -0500

    react to linting

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-require("electron-reload")(__dirname)
+require("electron-reload")(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 1280, height: 720, minWidth: 800, minHeight: 500})
+  mainWindow = new BrowserWindow({width: 1280, height: 720, minWidth: 800, minHeight: 500});
   
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
   
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 } 
   app.on('ready', createWindow)
   
   app.on('window-all-closed', function () {
       app.quit();
-  })
+  });
 
 
 // In this file you can include the rest of your app's specific main process
